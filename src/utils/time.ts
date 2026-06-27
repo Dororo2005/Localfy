6714d96aeb9b1edecf55cd0dcdf72c3e1b576435
RauMaKingDom36 <lamnhattv2022@gmail.com>
2026-06-27 13:28:44 +0700

    Update code

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,3 +3,18 @@ export const formatTime = (seconds: number) => {
   const minutes = Math.floor(seconds / 60)
   return `${minutes}:${Math.floor(seconds % 60).toString().padStart(2, '0')}`
 }
+function checkScore(score: number): string {
+  if (score >= 85) {
+    return '';
+  } else if (score >= 70) {
+    return '';
+  } else if (score >= 50) {
+    return '';
+  } else {
+    return '' ;
+  }
+}
+
+console.log(checkScore(90)); // Giỏi
+console.log(checkScore(72)); // Khá
+console.log(checkScore(40)); // Yếu
